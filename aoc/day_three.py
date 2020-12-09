dc1243d8c5e783fa3fc095b9b1642fb0aa09e40f
Rodolfo Puig <rodolfo@puig.io>
2020-12-09 05:48:45 -0500

    Adding comments to day 3

--- aoc/day_three.py
+++ aoc/day_three.py
@@ -3,8 +3,9 @@ from math import ceil
 
 
 def expand_grid(grid, offset):
+    # this part feels dirty, I need to find an optimal algorithm to grow the grid
     factor = ceil(len(grid) / offset.y) + 1
-    return [list(r) * factor for r in grid]
+    return [list(row) * factor for row in grid]
 
 
 class Node:
@@ -37,7 +38,6 @@ class Graph:
         self.offset = offset
         self.width = len(self.grid[0])
         self.height = len(self.grid)
-        self.visited = set()
         self.values = {}
 
     def value_of(self, node):
@@ -46,8 +46,10 @@ class Graph:
     def is_tree(self, node):
         return self.value_of(node) == self.TYPE_TREE
 
-    def walk(self, node, offset):
-        x, y = list(tuple(u + v for u, v in zip((node.x, node.y), offset)))
+    def walk(self, node):
+        # use a bit of matrix arithmetic to move our cursor
+        x, y = list(tuple(u + v for u, v in zip((node.x, node.y), self.offset)))
+        # try not to overflow our grid
         if self.width > x >= 0 and self.height > y >= 0:
             yield Node(x, y)
 
@@ -56,10 +58,10 @@ class Graph:
         queue.append(Node(0, 0))
 
         while queue:
+            # pop first node in our queue
             current = queue.popleft()
-            self.visited.add(current)
-
-            for next in self.walk(current, self.offset):
+            # find next node position
+            for next in self.walk(current):
                 self.values[next] = self.value_of(next)
                 queue.append(next)
 
@@ -67,6 +69,8 @@ class Graph:
 
 
 def answer(grid, offset):
+    # build graph from grid
     graph = Graph(grid, offset)
+    # find all trees in our offset path
     trees = [p for p in graph.travel() if graph.is_tree(p)]
     return len(trees)
